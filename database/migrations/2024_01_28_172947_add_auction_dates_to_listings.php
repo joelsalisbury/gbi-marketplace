65de50ddd41260535317d674a8db772deb2f8b72
Joel Salisbury <joel.salisbury@gmail.com>
2024-01-28 12:34:03 -0500

    resolves #1

---
+++ database/migrations/2024_01_28_172947_add_auction_dates_to_listings.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->dateTime('auction_start_date')->nullable();
+            $table->dateTime('auction_end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->dropColumn('auction_start_date');
+            $table->dropColumn('auction_end_date');
+        });
+    }
+};
