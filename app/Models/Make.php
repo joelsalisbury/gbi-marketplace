58a09c8d562f180ff1b6f131f2c5be0d20664079
Joel Salisbury <joel.salisbury@gmail.com>
2024-02-01 21:29:28 -0500

    stubbing out makes

--- app/Models/Make.php
+++ app/Models/Make.php
@@ -9,8 +9,33 @@ class Make extends Model
 {
     use HasFactory;
     
-    public function vehicleModels()
+    public function vehicleModel()
     {
         return $this->hasMany(VehicleModel::class, 'make_id');
+    }
+
+    public function index()
+    {
+        $makes = \App\Models\Make::all();
+        return view('makes.index', compact('makes'));
+    }
+
+    public function show(string $id)
+    {
+        $make = \App\Models\Make::find($id);
+        return view('makes.show', compact('make'));
+    }
+
+    public function create()
+    {
+        return view('makes.create');
+    }
+
+    public function store(Request $request)
+    {
+        $make = new \App\Models\Make;
+        $make->name = $request->name;
+        $make->save();
+        return redirect('/makes');
     }
 }
